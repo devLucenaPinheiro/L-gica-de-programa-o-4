6f0f074d718df9b063ffefe6deb1e53fb447fc84
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-06-20 16:05:36 -0300

    "tabuleiro"

--- listarray.js
+++ listarray.js
@@ -5,10 +5,10 @@ console.log(nick)
 
 let nomes = ["João","Vinícius","Rodrigo","Arthur"] //sintaxe literal
 
-let names = new Array("João","Vinícius","Rodrigo","Arthur") // sintaxe função construtora
+let names = new Array("João","Vinícius","Rodrigo","Arthur","Marco","Claudinê") // sintaxe função construtora
 
 
-for(let i = 0; i < 4; i++){
+for(let i = 0; i < names.length; i++){
     document.write("Nome: " + names[i] + "<br>")
 }
 
---
+++ o x.js
@@ -0,0 +1,9 @@
+let o = " o "
+let x = " x "
+
+for(let i = 0; i < 8; i++){
+    for(let i = 0; i < 8; i++){
+        document.write(o)
+    }
+    document.write(x + "<br>")
+}
