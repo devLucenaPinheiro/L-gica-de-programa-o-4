d183e4c1db2ecf9da99ea7a331ab1aff84c805d6
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-06-20 15:24:40 -0300

    Update listarray.js

--- listarray.js
+++ listarray.js
@@ -1,1 +1,7 @@
-let n = ["João","Vinícius","Rodrigo","Arthur"]
+let nick = prompt("Digite o seu nome")
+
+console.log(nick)
+
+let nomes = ["João","Vinícius","Rodrigo","Arthur"]
+
+console.log(nomes[0,1,2,3])
