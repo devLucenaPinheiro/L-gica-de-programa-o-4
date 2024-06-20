ae8a4e1239f91cfa091e2ea26bd9485281c8d2dd
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-06-20 16:18:36 -0300

    nomes.js

---
+++ nomes.js
@@ -0,0 +1,6 @@
+let nick = prompt("Digite um nome")
+
+let nomes = ["João"," Vinícius"," Arthur ", nick]
+
+document.write(nomes + "<br>")
+document.write("Nessa lista temos: " + nomes.length + " nomes")
