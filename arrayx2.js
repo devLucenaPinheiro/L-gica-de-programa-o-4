c79e41e7b4bc062c21d71cfa07dd2dc3e4686bf7
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-06-26 11:12:32 -0300

    Update arrayx2.js

--- arrayx2.js
+++ arrayx2.js
@@ -3,5 +3,10 @@ let num2x =[]
 
 for(let i = 0; i < num.length; i++){
     console.log(num[i] * 2)
+
+    //num2x = [num2x.length] = num
+    num2x.push(num, "alou", false) //Método push "empurra" um novo valor no final do array
 }
+console.log(num2x)
+
 console.log("Fim do loop")
