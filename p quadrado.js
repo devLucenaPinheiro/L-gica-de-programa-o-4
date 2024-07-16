0eccb14c03574858de34b9852c143846356f8ac6
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-07-15 23:40:53 -0300

    Update p quadrado.js

--- p quadrado.js
+++ p quadrado.js
@@ -1,3 +1,4 @@
 let lado = parseInt(prompt("Digite a medida do lado do quadrado"))
 
-alert("A área do quadrado é: " + lado**2 + " e o perimetro do quadrado é: " + lado*4)
+alert("A área do quadrado é: " + lado**2 + " e o perimetro do quadrado é: " + lado*4)]
+
