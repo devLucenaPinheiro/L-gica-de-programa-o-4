d01c4157831b9828090d51a4ee3413cb54b2ab36
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-07-16 22:24:36 -0300

    triangulo js
    triangulo js

---
+++ triangulo.js
@@ -0,0 +1,6 @@
+let altura = parseInt(prompt("Digite a altura do triangulo."))
+let base = parseInt(prompt("Digite a base do triangulo."))
+
+let area = base * altura / 2
+
+alert("O valor da área deste triangulo é: " + area)
