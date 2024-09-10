55c3d929b8acb9a422e48b35062e1f2ce08b3453
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-09-10 19:22:26 -0300

    update excluir_num.js
    solucionado o problema de excluir número de um array

--- excluir_num/excluir_num.js
+++ excluir_num/excluir_num.js
@@ -1,10 +1,14 @@
-const lista = [1, 3, 3, 1, 3, 2]
-let val = 3
+const lista = [1, 3, 3, 1, 3, 2, 1]
+let num = 3
 
-const listaFiltrada = lista.filter(val)
+const listaFiltrada = lista.filter(num => num !==3)
 
 let k = listaFiltrada.length
 
+const removidos = lista.length - k
 
 
-console.log("A lista filtrada é:" + listaFiltrada)
+
+console.log("A lista filtrada é: " + listaFiltrada)
+console.log("A quantidade de números que sobrou foi " + k)
+console.log("E a quantidade de números removidos foi de: " + removidos)
