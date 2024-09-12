f3db3e68eb9c62e3043a7076fb2dff1bbd605d40
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-09-11 22:08:20 -0300

    Update exemplo.js

--- excluir_num/exemplo.js
+++ excluir_num/exemplo.js
@@ -1,13 +1,13 @@
-num = 3
-lista = [1,2,3]
+const lista = [1, 3, 3, 1, 3, 2, 1, 4, 4] //declara uma lista 
+let num =(prompt("Digite um número")) //pede o número (que vai ser utilizado para ser manipulado na função)
 
-const listaFiltrada = lista.filter(function(num){
-if(num !==3){
-    return true
-}
-else{
-    return false
-}
-})
+const listaFiltrada = lista.filter(num1 => num != num1)
 
-console.log(listaFiltrada)
+
+let k = listaFiltrada.length // K vai ser apenas utilizado para falar quantos números sobraram
+
+const removidos = lista.length - k //removidos vai dizer quantos foram removidos
+
+console.log("A lista filtrada é: " + listaFiltrada) //faz os resultados aparecerem na tela
+console.log("A quantidade de números que sobrou foi " + k) //faz os resultados aparecerem na tela
+console.log("E a quantidade de números removidos foi de: " + removidos) //faz os resultados aparecerem na tela
