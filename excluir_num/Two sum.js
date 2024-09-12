da827656a8226a3132f8d30d152e01ef87f984da
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-09-12 14:14:12 -0300

    Soma

---
+++ excluir_num/Two sum.js
@@ -0,0 +1,10 @@
+let target = prompt("Digite um número")
+
+nums = [2, 3, 6, 11]
+
+let soma = function(nums, target){
+    if(target == nums + nums)
+        return true
+}
+
+console.log("A soma foi efetuada por")
