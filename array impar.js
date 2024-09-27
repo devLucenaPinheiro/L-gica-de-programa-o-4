bd23ed76084d94fd81023bf1aa723e1295cddc6a
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-09-27 20:34:26 -0300

    exercícios de array

--- array impar.js
+++ array impar.js
@@ -1,14 +1,13 @@
-let array = [2, null, 4, 1, "oi", false, 3, 7, true]
+const array =[1,2,3,4,5,6,7]
 
 let soma = 0
 
 for(i=0;i<array.length;i++){
-
     let n = array[i]
-    if(typeof n === "number"){
+    if(array[i]%2 == 0){
+    }
+    else{
         soma = soma + n
     }
-
 }
-
 console.log(soma)
---
+++ array meio.js
@@ -0,0 +1,5 @@
+const array =[1, 2, 3, 4, 5, 6, 7]
+
+let meio = parseInt(array.length/2)
+
+console.log(array[meio])
